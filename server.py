5892e6a927f24df24f8fa06d6623b9bb84d311ec
Bony Joshy <14826333+bonyjoshy@users.noreply.github.com>
2026-07-23 07:28:43 -0400

    Created server.py file

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+@app.route("/emotionDetector")
+def emotion_detector_server():
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+    label_dominant_emotion = response.pop("dominant_emotion")
+    key, value = response.popitem()
+    last_emotion = {key: value}
+
+    return f"For the given statement, the system response is \
+    {str(response).replace('{', '').replace('}', '')} and \
+    {str(last_emotion).replace('{', '').replace('}', '')}. \
+    The dominant emotion is {label_dominant_emotion}."
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000) 
